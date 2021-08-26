30cc1e4c90eb45b2c9b58f1a1c013443e09b004e
Adam-cm <70616298+Adam-cm@users.noreply.github.com>
2021-08-27 01:45:52 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -112,6 +112,11 @@ int main () {
       if (packetSize) {
         // received a packet
         printf("Packet Received\n");
+        string message = "";        // Clear message string
+        while (LoRa.available()) {
+          message = message + ((char)LoRa.read());
+        }
+        printf("Message Stored");
       }
     }
 
