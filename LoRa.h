1ab490a7e8b44b903e4229d042e1636e72b35d7c
Adam-cm <70616298+Adam-cm@users.noreply.github.com>
2021-08-26 19:36:19 +1000

    String Stream

--- LoRa.h
+++ LoRa.h
@@ -4,7 +4,7 @@
 #ifndef LORA_H
 #define LORA_H
 
-#include "platform/Stream.h"
+#include "Stream.h"
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
 
--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@
 #include <cstring>
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <sstream>
 
 using namespace std;
 
