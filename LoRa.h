7a49ded88b2dd27aaa9df95c411941d02a8ec46d
Adam-cm <70616298+Adam-cm@users.noreply.github.com>
2021-08-26 22:47:55 +1000

    Changed

--- LoRa.h
+++ LoRa.h
@@ -49,7 +49,7 @@ public:
 
   // from Print
   virtual size_t write(uint8_t byte);
-  virtual size_t write(const uint8_t *buffer, size_t size);
+  virtual size_t write(const char *buffer, size_t size);
 
   // from Stream
   virtual int available();
