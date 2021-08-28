45049d1d73cc1e1edaaf8cdbc342359ff6eb613d
Adam-cm <70616298+Adam-cm@users.noreply.github.com>
2021-08-29 01:49:22 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -169,8 +169,8 @@ int main () {
         exit(EXIT_FAILURE);
     }
 
-    pubmsg.payload = PAYLOAD;
-    pubmsg.payloadlen = (int)strlen(PAYLOAD);
+    pubmsg.payload = (char *)PAYLOAD.c_str();
+    pubmsg.payloadlen = (int)strlen((char *)PAYLOAD.c_str());
     pubmsg.qos = QOS;
     pubmsg.retained = 0;
     if ((rc = MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token)) != MQTTCLIENT_SUCCESS)
@@ -181,7 +181,7 @@ int main () {
 
     printf("Waiting for up to %d seconds for publication of %s\n"
             "on topic %s for client with ClientID: %s\n",
-            (int)(TIMEOUT/1000), (PAYLOAD, TOPIC, CLIENTID);
+            (int)(TIMEOUT/1000), ((char *)PAYLOAD.c_str(), TOPIC, CLIENTID);
     rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
     printf("Message with delivery token %d delivered\n", token);
 
